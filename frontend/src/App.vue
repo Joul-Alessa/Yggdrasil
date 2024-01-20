5fa1ebabd4325f6f1502a1c6577959b6f845a2a3
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2024-01-19 20:23:01 -0600

    Closing uncommitted changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,24 @@
 <script setup>
+var language = "";
+
 import HelloWorld from './components/HelloWorld.vue'
+import Introduction from './components/Introduction.vue'
 </script>
 
 <template>
+    <p>Choose your language:</p>
+
+    <form>
+        <input type="radio" id="en" name="language" value="en" v-model="language">
+        <label for="en">English</label><br>
+        <input type="radio" id="es" name="language" value="es" v-model="language">
+        <label for="es">Spanish</label><br>
+    </form>
+
+    <p>{{ language }}</p>
+
+  <Introduction />
+  <Introduction />
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
---
+++ frontend/src/components/Introduction.vue
@@ -0,0 +1,11 @@
+<script setup>
+var hola = 12;
+</script>
+
+<template>
+  <p>Wenas wenas {{ hola }}</p>
+</template>
+
+<style scoped>
+
+</style>
