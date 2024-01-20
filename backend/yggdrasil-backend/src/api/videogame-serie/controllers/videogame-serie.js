504b0ed64a6ad8d48e4baca307d8f7d0d4626c4a
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2024-01-19 22:53:51 -0600

    Inicialized Strapi backend and added videogame and videogame series endpoint

---
+++ backend/yggdrasil-backend/src/api/videogame-serie/controllers/videogame-serie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * videogame-serie controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::videogame-serie.videogame-serie');
---
+++ backend/yggdrasil-backend/src/api/videogame-serie/routes/videogame-serie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * videogame-serie router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::videogame-serie.videogame-serie');
